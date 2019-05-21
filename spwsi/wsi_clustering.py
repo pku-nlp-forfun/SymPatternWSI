849df6fc9a98d2cfdb1eeccbd8f027746f738ca6
gunjianpan <iofu728@163.com>
2019-05-22 00:43:04 +0800

    🚖gloved embed

--- spwsi/wsi_clustering.py
+++ spwsi/wsi_clustering.py
@@ -7,13 +7,30 @@ from collections import Counter
 import logging
 from pytorch_pretrained_bert import BertModel, BertTokenizer
 import torch
+import numpy as np
 
 bert_dir = '/Users/gunjianpan/Desktop/git/bert'
 bert = BertModel.from_pretrained(bert_dir)
 tokenizer = BertTokenizer.from_pretrained(f'{bert_dir}/uncased_L-12_H-768_A-12/vocab.txt')
+gloved_path = '../glove.840B.300d.txt'
+gloved_embed = load_embedding(gloved_path)
+
+def load_embedding(data_path:str) -> Dict[str, List[float]]:
+    ''' load embedding '''
+    with open(data_path) as f:
+        origin_embed = [ii.strip() for ii in f.readlines()]
+    embed = {ii.split(' ')[0]: np.array(ii.split(' ')[1:]).astype(float) for ii in origin_embed}
+    return embed
+
+def get_embed(word:str)->List[float]:
+    if word in gloved_embed:
+        return gloved_embed[word]
+    else:
+        return np.zeros(300)
+
 
 def cluster_inst_ids_representatives(inst_ids_to_representatives: Dict[str, List[Dict[str, int]]],
-                                     n_clusters: int, disable_tfidf: bool, useBert:bool=True) -> Dict[str, Dict[str, int]]:
+                                     n_clusters: int, disable_tfidf: bool, embedType:int=2) -> Dict[str, Dict[str, int]]:
     """
     preforms agglomerative clustering on representatives of one SemEval target
     :param inst_ids_to_representatives: map from SemEval instance id to list of representatives
@@ -27,12 +44,14 @@ def cluster_inst_ids_representatives(inst_ids_to_representatives: Dict[str, List
     representatives = [y for x in inst_ids_ordered for y in inst_ids_to_representatives[x]]
     n_represent = len(representatives) // len(inst_ids_ordered)
     to_pipeline = [DictVectorizer()]
-    if useBert:
+    if embedType == 1:
         waitSentence = [' '.join(ii) for ii in representatives]
         transformed = []
         for ii in waitSentence:
             ids = torch.tensor([tokenizer.convert_tokens_to_ids(tokenizer.tokenize(ii))])
             transformed.append(bert(ids, output_all_encoded_layers=False)[-1][0].detach().numpy())
+    elif embedType == 2:
+        transformed = [sum([get_embed(jj) for jj in ii]) for ii in representatives]
     else:
         if not disable_tfidf:
             to_pipeline.append(TfidfTransformer())
